94607a42b3e074d10852e39a19753ea2d670f54a
Keng1ne <StyXinator98@googlemail.com>
2022-03-20 21:43:50 +0100

    added options for parameters

--- lua/telescope/_extensions/template_actions/actions.lua
+++ lua/telescope/_extensions/template_actions/actions.lua
@@ -5,15 +5,34 @@ local M = {}
 
 M.execute_method = function(prompt_bufnr)
   local method = actions_state.get_selected_entry(prompt_bufnr)
+  method.params = method.params or {}
   local params = {}
 
-  for _, value in pairs(method.params) do
-    local input = vim.fn.input("Input for " .. value .. ": ", "") or ""
+  for paramName, paramOptions in pairs(method.params) do
+    local param = paramOptions.text or paramName
+    
+    local input 
+    if paramOptions.type then
+      input =  vim.fn.input(param .. ": ", "", paramOptions.type or "none") or ""
+    else
+      input =  vim.fn.input(param .. ": ", "") or ""
+    end
+
     if not input or input == "" then
-      error("Missing " .. value .. " Parameter")
+      error("Missing " .. paramName .. " Parameter")
       return
     end
-    params[value] = input
+
+    local validator = paramOptions.validator
+    if validator then
+      local valid = validator(input)
+      print(valid)
+      if not valid then
+	error(input .. " doesn't match requirements for parameter " .. paramName)
+	return
+      end
+    end
+    params[paramName] = input
   end
 
   if not params then
@@ -25,12 +44,12 @@ M.execute_method = function(prompt_bufnr)
   end
 
   Job
-    :new({
-      command = method.executable,
-      cwd = params.cwd,
-      args = method.args,
-    })
-    :sync()
+  :new({
+    command = method.executable,
+    cwd = params.cwd,
+    args = method.args,
+  })
+  :sync()
 
   if type(method.post) == "function" then
     method.post(method, params)
